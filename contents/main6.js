8cc707c27418770f934f90177f98c610a38a6a4a
ekasai6724 <87635825+ekasai6724@users.noreply.github.com>
2022-01-04 07:19:05 +0900

    Update main6.js

--- contents/main6.js
+++ contents/main6.js
@@ -110,11 +110,11 @@ document.body.addEventListener('keydown', event => {
 // ゲームパッド接続イベント
 window.addEventListener("gamepadconnected", (e) => {
     console.log(
-        "Gamepad connected at index %d: %s. %d buttons, %d axes.",
-        e.gamepad.index,
-        e.gamepad.id,
-        e.gamepad.buttons.length,
-        e.gamepad.aexs.length
+      "Gamepad connected at index %d: %s. %d buttons, %d axes.",
+      e.gamepad.index,
+      e.gamepad.id,
+      e.gamepad.buttons.length,
+      e.gamepad.axes.length
     );
 });
 
