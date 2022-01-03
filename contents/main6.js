7b1033fc22d2af93ff043d4781e171e3ea656485
ekasai6724 <87635825+ekasai6724@users.noreply.github.com>
2022-01-03 17:03:46 +0900

    ゲームパッド接続イベントを追加

--- contents/main6.js
+++ contents/main6.js
@@ -4,106 +4,124 @@ ros.on('connection', function() {console.log('websocket: connected'); });
 ros.on('error', function(error) {console.log('websocket error: ', error); });
 ros.on('close', function() {console.log('websocket: closed');});
 
+// 距離センサデータ読出トピックオブジェクト
 var ls = new ROSLIB.Topic({
-        ros : ros,
-        name : '/lightsensors',
-        messageType : 'pimouse_ros/LightSensorValues'
+    ros : ros,
+    name : '/lightsensors',
+    messageType : 'pimouse_ros/LightSensorValues'
 });
 
+// 距離センサデータ受信時に実行:距離データ表示
 ls.subscribe(function(message) {
-        for( e in message ){
-                document.getElementById(e).innerHTML = message[e];
-        }
+    for( e in message ){
+        document.getElementById(e).innerHTML = message[e];
+    }
 });
 
-//距離センサの処理とカメラの処理の間に以下のコードを記述
+// モータ励磁オンサービスオブジェクト
 var on = new ROSLIB.Service({
-        ros : ros,
-        name : '/motor_on',
-        messageType : 'std_srvs/Trigger'
+    ros : ros,
+    name : '/motor_on',
+    messageType : 'std_srvs/Trigger'
 });
 
+// モータ励磁オフサービスオブジェクト
 var off = new ROSLIB.Service({
-        ros : ros,
-        name : '/motor_off',
-        messageType : 'std_srvs/Trigger'
+    ros : ros,
+    name : '/motor_off',
+    messageType : 'std_srvs/Trigger'
 });
 
+// モータ励磁オンボタンクリック
 $('#motor_on').on('click', function(e){
-        on.callService(ROSLIB.ServiceRequest(),function(result){
-                if(result.success){
-                        $('#motor_on').attr('class','btn btn-danger');
-                        $('#motor_off').attr('class','btn btn-default');
-                }
-        });
-
+    on.callService(ROSLIB.ServiceRequest(),function(result){
+        if(result.success){
+            $('#motor_on').attr('class','btn btn-danger');
+            $('#motor_off').attr('class','btn btn-default');
+        }
+    });
 });
-        
+
+// モータ励磁オフボタンクリック
 $('#motor_off').on('click', function(e){
-        off.callService(ROSLIB.ServiceRequest(),function(result){
-                if(result.success){
-                        $('#motor_on').attr('class','btn btn-default');
-                        $('#motor_off').attr('class','btn btn-primary');
-                }
-        });
+    off.callService(ROSLIB.ServiceRequest(),function(result){
+        if(result.success){
+            $('#motor_on').attr('class','btn btn-default');
+            $('#motor_off').attr('class','btn btn-primary');
+        }
+    });
 });
 
+// ROSのモータ速度データオブジェクト
 var vel = new ROSLIB.Topic({
-        ros : ros,
-        name : '/cmd_vel',
-        messageType : 'geometry_msgs/Twist'
+    ros : ros,
+    name : '/cmd_vel',
+    messageType : 'geometry_msgs/Twist'
 });
 
+// ROSにモータ速度データを発行する
 function pubMotorValues(){
-        fw = $('#vel_fw').html();
-        rot = $('#vel_rot').html();
+    fw = $('#vel_fw').html();
+    rot = $('#vel_rot').html();
 
-        fw = parseInt(fw)*0.001;
-        rot = 3.141592*parseInt(rot)/180;
-        v = new ROSLIB.Message({linear:{x:fw,y:0,z:0}, angular:{x:0,y:0,z:rot}});
-        vel.publish(v);
+    fw = parseInt(fw)*0.001;
+    rot = 3.141592*parseInt(rot)/180;
+    v = new ROSLIB.Message({linear:{x:fw,y:0,z:0}, angular:{x:0,y:0,z:rot}});
+    vel.publish(v);
 }
+setInterval(pubMotorValues,100);
 
+// モータ速度操作パッドクリック
 $('#touchmotion').on('click', function(e){
-        rect = $('#touchmotion')[0].getBoundingClientRect();
-        x = e.pageX - rect.left - window.pageXOffset;
-        y = e.pageY - rect.top - window.pageYOffset;
+    rect = $('#touchmotion')[0].getBoundingClientRect();
+    x = e.pageX - rect.left - window.pageXOffset;
+    y = e.pageY - rect.top - window.pageYOffset;
         
-        vel_fw = (rect.height/2 - y)*3;
-        vel_rot = rect.width/2 - x;
-        $('#vel_fw').html(parseInt(vel_fw));
-        $('#vel_rot').html(parseInt(vel_rot));
+    vel_fw = (rect.height/2 - y)*3;
+    vel_rot = rect.width/2 - x;
+    $('#vel_fw').html(parseInt(vel_fw));
+    $('#vel_rot').html(parseInt(vel_rot));
 });
 
 // キーボードの矢印キー入力
 document.body.addEventListener('keydown', event => {
-        switch (event.key) {
-                case 'ArrowUp':
-                        vel_fw = 100;
-                        vel_rot = 0;
-                        break;
-                case 'ArrowDown':
-                        vel_fw = -100;
-                        vel_rot = 0;
-                        break;
-                case 'ArrowLeft':
-                        vel_fw = 0;
-                        vel_rot = 30;
-                        break;
-                case 'ArrowRight':
-                        vel_fw = 0;
-                        vel_rot = -30;
-                        break;
-        }
-        $('#vel_fw').html(parseInt(vel_fw));
-        $('#vel_rot').html(parseInt(vel_rot));
+    switch (event.key) {
+        case 'ArrowUp':
+            vel_fw = 100;
+            vel_rot = 0;
+            break;
+        case 'ArrowDown':
+            vel_fw = -100;
+            vel_rot = 0;
+            break;
+        case 'ArrowLeft':
+            vel_fw = 0;
+            vel_rot = 30;
+            break;
+        case 'ArrowRight':
+            vel_fw = 0;
+            vel_rot = -30;
+            break;
+    }
+    $('#vel_fw').html(parseInt(vel_fw));
+    $('#vel_rot').html(parseInt(vel_rot));
 });
 
-setInterval(pubMotorValues,100);
+// ゲームパッド接続イベント
+window.addEventListener("gamepadconnected", (e) => {
+    console.log(
+        "Gamepad connected at index %d: %s. %d buttons, %d axes.",
+        e.gamepad.index,
+        e.gamepad.id,
+        e.gamepad.buttons.length,
+        e.gamepad.aexs.length
+    );
+});
 
+// USBカメラ画像表示
 document.getElementById('camstream').data = 'http://'
-        + location.hostname
-        + ':10000/stream?topic=/cv_camera_node/image_raw';
+                                            + location.hostname
+                                            + ':10000/stream?topic=/cv_camera_node/image_raw';
 
 // Copyright 2016 Ryuichi Ueda
 // Released under the MIT License.
